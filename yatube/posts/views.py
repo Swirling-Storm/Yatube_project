a0dadd331ef7fbe95b0ff5531e1176377a17677c
Егор Кабитов <kabitov.egor@ya.ru>
2023-02-17 18:32:34 +0300

    Add urls and view-functions

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse('Тут типа главная страница')
+
+
+def groups_posts(request, slug):
+    return HttpResponse(f'А тут типа пост {slug}')
