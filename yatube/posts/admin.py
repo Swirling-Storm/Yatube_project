06dfe21cfcd2f503d8239e88cf349c9d48741591
Егор Кабитов <kabitov.egor@ya.ru>
2023-02-19 20:35:15 +0300

    Prefinal

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
     # Перечисляем поля, которые должны отображаться в админке
-    list_display = ('pk', 'text', 'pub_date', 'author')
+    list_display = ('pk',
+                    'text',
+                    'pub_date',
+                    'author',
+                    'group'
+                )
+    list_editable = ('group',)
     # Добавляем интерфейс для поиска по тексту постов
     search_fields = ('text',)
     # Добавляем возможность фильтрации по дате
@@ -18,3 +24,4 @@ class PostAdmin(admin.ModelAdmin):
 # При регистрации модели Post источником конфигурации для неё назначаем
 # класс PostAdmin
 admin.site.register(Post, PostAdmin)
+admin.site.register(Group)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,23 +1,21 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 # Create your views here.
 def index(request):
-    title = 'Это главная страница проекта Yatube'
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.order_by("-pub_date")[:10]
     context = {
-        'title': title,
         'posts': posts,
     }
     return render(request, 'posts/index.html', context)
 
 
 def groups_posts(request, slug):
-    template = 'posts/group_list.html'
-    title = 'Лев Толстой – зеркало русской революции.'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by("-pub_date")[:10]
     context = {
-        'title': title,
-        'text': 'Здесть будет информация о группах проекта'
+        'group': group,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, "posts/group_list.html", context)
