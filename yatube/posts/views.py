e943534981272ae7efa6bfdd4a8d6bae3fd87785
Егор Кабитов <kabitov.egor@ya.ru>
2023-02-19 19:31:48 +0300

    Add file db.sqlite3 for another posts

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
 def index(request):
-    template = 'posts/index.html'
-    title = 'Последние обновления на сайте'
+    title = 'Это главная страница проекта Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'title': title,
-        'text': 'Это главная страница проекта Yatube'
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def groups_posts(request, slug):
